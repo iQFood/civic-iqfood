70b56cba170e1cb0846944a1b8d91fef8766b109
Swager0932 <swagerbot2000@gmail.com>
2023-06-30 05:05:49 -0400

    Final Styling coices for DEMO++

--- frontend/src/pages/LoadingPage.jsx
+++ frontend/src/pages/LoadingPage.jsx
@@ -3,9 +3,9 @@ import load from "../media/recipes.gif";
 export default function LoadingPage(){
     return(
         <div id="load-page">
-            <h1>Hold on!</h1>
+            <h1>Hold on...</h1>
             <img id="load-page-img" src={load}></img>
-            <h1>We're cooking up this page :) </h1>
+            <h1>We're cooking in the back to bring you your request :) </h1>
         </div>
     );
 }
